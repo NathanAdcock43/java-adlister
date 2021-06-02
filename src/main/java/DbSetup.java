c672d6ba319b1e3fe372af76aafc2a1e2dbb751b
Nathan Adcock <nathan.adcock43@gmail.com>
2021-06-02 11:02:38 -0500

    Feat: mysqlproductdao

--- src/main/java/DbSetup.java
+++ src/main/java/DbSetup.java
@@ -4,7 +4,7 @@ import com.mysql.cj.jdbc.Driver;
 
 
 public class DbSetup {
-
+//connection object
     public static void main(String[] args) throws SQLException {
         DriverManager.registerDriver(new Driver());
         Connection connection = DriverManager.getConnection
@@ -13,5 +13,35 @@ public class DbSetup {
                 "root",
                 "codeup"
         );
+
+
+//statement object (initialize it)
+    Statement statement = connection.createStatement();
+
+    ResultSet selectResults = statement.executeQuery("SELECT * FROM products");
+
+    while(selectResults.next()){
+        System.out.println("Here's a product:");
+        System.out.println("name : " + selectResults.getString("name") + "\n");
+        System.out.println("price: $" + selectResults.getInt("price") + "\n");
+
+        String insertQuery = "INSERT INTO products (name, price) VALUE ('nailgun', 250)";
+
+        Statement insertStatement = connection.createStatement();
+
+
+
+        insertStatement.executeUpdate(insertQuery, Statement.RETURN_GENERATED_KEYS);
+        ResultSet insertResults = insertStatement.getGeneratedKeys();
+
+        if(insertResults.next()){
+            System.out.println("Administrator: A new item has been created with the id of : " + insertResults.getLong(1));
+        }
     }
+    }
+
+
+
+
+
 }
